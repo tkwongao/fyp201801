bd350062a25c898fc07da66b5fbdd5f856618d8b
user <user@windows10.mshome.net>
2018-01-20 17:01:10 +0800

    People counting; Stay time in mall

--- fyp/src/fyp/Database.java
+++ fyp/src/fyp/Database.java
@@ -2,18 +2,34 @@ package fyp;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.TimeZone;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Objects;
+import java.util.TimeZone;
 
 public class Database {
-	private final String url = "jdbc:postgresql://143.89.50.151:7023/fypps";
-	private final String user = "fyp";
-	private final String password = "123456";
+	private final String url;
+	private final String user;
+	private final String password;
+
+	public Database() {
+		this("jdbc:postgresql://143.89.50.151:7023/fypps", "fyp", "123456");
+	}
+
+	/**
+	 * @param url The JDBC URL of the database
+	 * @param user The username to access the database
+	 * @param password The database password
+	 */
+	private Database(String url, String user, String password) {
+		this.url = url;
+		this.user = user;
+		this.password = password;
+	}
 
 	/**
 	 * Connect to the PostgreSQL database
@@ -29,57 +45,125 @@ public class Database {
 		} catch (SQLException e) {
 			System.out.println(e.getMessage());
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return conn;
 	}
 
-	/**
-	 * @param args the command line arguments
-	 */
-
-	public int count()
-	{
-		int count =0;
+	public int count() {
+		int count = 0;
 		String sql = "SELECT count(DISTINCT(did)) from store_results";
 		Connection connected = this.connect();
-		try
-		{
+		try {
 			PreparedStatement ps = connected.prepareStatement(sql);
 			ResultSet rs = ps.executeQuery();
-			while (rs.next()){
-				count= rs.getInt("count");
-			}
+			while (rs.next())
+				count = rs.getInt("count");
 		} catch (SQLException e) {
 			System.out.println(e.getMessage());
 		}
-		method();
-		return count; 
-	}
-	public void method() {
-		Calendar c = Calendar.getInstance(TimeZone.getTimeZone("GMT+8:00"));
-		c.clear();
-		c.set(2017, Calendar.OCTOBER, 23, 22, 0, 0);
-		long time1 = c.getTimeInMillis();
-		c.set(2017, Calendar.OCTOBER, 23, 0, 0, 0);
-		long time2 = c.getTimeInMillis();
-		ArrayList<Date> list = new ArrayList<Date>();
+
 		try {
-			ResultSet rs = this.connect().prepareStatement("SELECT startts FROM store_results where startts > " + time1 + " ORDER BY startts LIMIT 400;").executeQuery();
-			long start = System.nanoTime();
-			while (rs.next())
-				list.add(new Date(rs.getLong("startts")));
-			double elapsedTimeInSec = (System.nanoTime() - start) * 1.0e-9;
-			System.out.println(elapsedTimeInSec);
-			rs = this.connect().prepareStatement("SELECT startts FROM store_results where startts > " + time2 + " ORDER BY startts").executeQuery();
-			start = System.nanoTime();
-			while (rs.next())
-				list.add(new Date(rs.getLong("startts")));
-			double elapsedTimeInSec2 = (System.nanoTime() - start) * 1.0e-9;
-			System.out.println(elapsedTimeInSec2);
+			System.out.println(peopleCounting(time2Period(2017, 10, 23, 22, null, null), null));
 		} catch (SQLException e) {
-			System.out.println(e.getMessage());
+			e.printStackTrace();
 		}
+		return count;
+	}
+
+	/**
+	 * Connect the database to count the number of devices detected in the whole mall or a particular store in a certain period of time.
+	 * @param period The earliest and latest times the counted data could come from, as UTC milliseconds from the epoch.
+	 * @param storeID The ID of the store. If <code>null</code>, the mall level data will be counted.
+	 * @return A non-negative <code>int</code> representing the count in the specific time range.
+	 * @throws SQLException if an error occurs on database operations.
+	 */
+	public int peopleCounting(final Long[] period, final Integer storeID) throws SQLException {
+		if (Objects.isNull(period) || Objects.isNull(period[0]) || Objects.isNull(period[1]))
+			throw new NullPointerException();
+		long count = -1;
+		String sql = (Objects.isNull(storeID)) ? 
+				"SELECT count(DISTINCT(DID)) FROM site_results WHERE startts > ? AND startts < ?" :
+					"SELECT count(DISTINCT(DID)) FROM store_results WHERE startts > ? AND startts < ? AND storeid = ?";
+		PreparedStatement ps = this.connect().prepareStatement(sql);
+		ps.setLong(1, period[0]); // Use parameterized statement to prevent SQL injection.
+		ps.setLong(2, period[1]);
+		if (Objects.nonNull(storeID))
+			ps.setInt(3, storeID);
+		ResultSet rs = ps.executeQuery();
+		while (rs.next())
+			count = rs.getLong("count");
+		if (count < 0)
+			throw new SQLException("Some exceptions have occurred.");
+		return (int) count;
+	}
+
+	/**
+	 * Convert a human-readable date to UTC milliseconds from the epoch, optionally as a period of a second, a minute, an hour,
+	 * a day, a month or a year. The human-readable date is in GMT+8:00 (Hong Kong time) time zone.
+	 * @param year The year. Cannot be <code>null</code>.
+	 * @param month The month. If <code>null</code>, the period will contain the whole year.
+	 * @param day The day. If <code>null</code>, the period will contain the whole month.
+	 * @param hour The hour. If <code>null</code>, the period will contain the whole day.
+	 * @param minute The minute. If <code>null</code>, the period will contain the whole hour.
+	 * @param second The second. If <code>null</code>, the period will contain the whole minute.
+	 * @return A <code>Long</code> array with length 2, with its first element the start time, and the second one the end time.
+	 * The second element is optional.
+	 */
+	public Long[] time2Period(Integer year, Integer month, Integer day, Integer hour, Integer minute, Integer second) {
+		Calendar c = new GregorianCalendar(TimeZone.getTimeZone("GMT+8:00"));
+		c.clear();
+		c.setLenient(false);
+		Long[] time = new Long[2];
+		if (Objects.nonNull(month))
+			month--; // month -1 to fit with the month definition in the Calendar class.
+		try {
+			// throws IllegalArgumentException if year, month, day, hour, minute or second is out of range.
+			if (year < 2015)
+				throw new IllegalArgumentException(year.toString()); // The system was not there such early...... this means invalid data
+			if (Objects.nonNull(second)) {
+				c.set(year, month, day, hour, minute, second);
+				time[0] = c.getTimeInMillis(); 
+				c.add(Calendar.SECOND, 1);
+			} else if (Objects.nonNull(minute)) {
+				c.set(year, month, day, hour, minute, 0);
+				time[0] = c.getTimeInMillis(); 
+				c.add(Calendar.MINUTE, 1);
+			} else if (Objects.nonNull(hour)) {
+				c.set(year, month, day, hour, 0, 0);
+				time[0] = c.getTimeInMillis(); 
+				c.add(Calendar.HOUR_OF_DAY, 1);
+			} else if (Objects.nonNull(day)) {
+				c.set(year, month, day, 0, 0, 0);
+				time[0] = c.getTimeInMillis(); 
+				c.add(Calendar.DAY_OF_YEAR, 1);
+			} else if (Objects.nonNull(month)) {
+				c.set(year, month, 0, 0, 0, 0);
+				time[0] = c.getTimeInMillis(); 
+				c.add(Calendar.MONTH, 1);
+			} else if (Objects.nonNull(year)) {
+				c.set(year, 0, 0, 0, 0, 0);
+				time[0] = c.getTimeInMillis(); 
+				c.add(Calendar.YEAR, 1);
+			} else
+				throw new NullPointerException();
+			time[1] = c.getTimeInMillis();
+		} catch (RuntimeException e) {
+			e.printStackTrace();
+		}
+		return time;
+	}
+	
+	public void stayTimeInMall(long macAddress) throws SQLException {
+		int totalSecond = 0;
+		String sql = "SELECT startts, endts FROM store_results WHERE did = ?";
+		PreparedStatement ps = this.connect().prepareStatement(sql);
+		ps.setLong(1, macAddress);
+		ResultSet rs = ps.executeQuery();
+		while (rs.next()) {
+			long start = rs.getLong("startts"), end = rs.getLong("endts");
+			totalSecond += (int) ((end - start) / 1000);
+		}
+		System.out.println(totalSecond + "seconds");
 	}
 }
