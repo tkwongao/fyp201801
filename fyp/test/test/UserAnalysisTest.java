5fe139090c5b15fc94585b21e64d2fc66338d658
ktleungaf <ktleungaf@csl1wk03.win2k.cs.ust.hk>
2018-03-06 16:59:11 +0800

    test cases updated, not completed

---
+++ fyp/test/test/UserAnalysisTest.java
@@ -0,0 +1,44 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class UserAnalysisTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testUserAnalysis() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUserStayTime() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLoyaltyCheck() {
+		fail("Not yet implemented");
+	}
+
+}
